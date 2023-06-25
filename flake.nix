eadbe8267d2890de684df2d6de3030fde3dd69fe
Luca Leon Happel <lucahappel99@gmx.de>
2023-06-25 17:52:38 +0200

    add: flake support

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "my project description";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = import ./default.nix { inherit pkgs; };
+      }
+    );
+}
