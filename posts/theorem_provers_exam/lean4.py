95ef8cb942dc7dd3a697e538fbe35f40882a6983
Luca Leon Happel <lucahappel99@gmx.de>
2024-02-22 07:35:45 +0100

    add: some more lean stuffy

--- posts/theorem_provers_exam/lean4.py
+++ posts/theorem_provers_exam/lean4.py
@@ -6,16 +6,19 @@ from pygments.lexers import Lean4Lexer
 from pygments.formatters import HtmlFormatter
 import subprocess
 
+
 def pygments(key, value, format, meta):
-    if key == 'CodeBlock':
+    if key == "CodeBlock":
         [[ident, classes, keyvals], code] = value
-        if any('lean4' in s for s in classes):
+        if any("lean4" in s for s in classes):
             lexer = Lean4Lexer()
             formatter = HtmlFormatter(
-                style='colorful',
+                style="default",
                 noclasses=True,
+                wrapcode=True,
             )
-            return RawBlock('html', highlight(code, lexer, formatter))
+            return RawBlock("html", highlight(code, lexer, formatter))
+
 
 if __name__ == "__main__":
     toJSONFilter(pygments)
