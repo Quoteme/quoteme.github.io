47dcc3b8b67a4abe98065b18c53b79018f773174
Luca Leon Happel <lucahappel99@gmx.de>
2026-03-06 08:33:53 +0100

    add: initial version of commit plotter
    refactor: code split into two cells
    
    add file graph
    
    add: some post related commentary by myself
    
    add: finished post
    
    add: final rewording
    
    refactor: move url for post
    
    refactor: move url for post

--- flake.nix
+++ flake.nix
@@ -9,10 +9,30 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pythonPackages = ps: with ps; [ plotly numpy pandas matplotlib jupyter-cache ];
+        pythonPackages =
+          ps: with ps; [
+            plotly
+            numpy
+            pandas
+            matplotlib
+            jupyter-cache
+            gitpython
+            ruff
+            mypy
+            ipython
+            ipykernel
+            pip
+          ];
         pkgs = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
@@ -28,29 +48,48 @@
         dependencies = with pkgs; [
           quarto
           git
-          (python3.withPackages pythonPackages)
+          # (python3.withPackages pythonPackages)
+          uv
         ];
-      in {
-        # default shell 
-        devShells.default = with pkgs;
+      in
+      {
+        # default shell
+        devShells.default =
+          with pkgs;
           mkShell {
             name = "blog-shell";
+            QUARTO_PYTHON = "./.venv/bin/python";
             buildInputs = dependencies;
+            shellHook = /* bash */ ''
+              #!/usr/bin/env bash
+              # check if this shell hook has been run before
+              if [ -z "$RAN_SHELL_HOOK_BASIC" ]; then
+                export RAN_SHELL_HOOK_BASIC=1
+              else
+                return
+              fi
+              ${pkgs.onefetch}/bin/onefetch
+              uv sync
+              source .venv/bin/activate
+              trap deactivate EXIT
+            '';
           };
 
         # build quarto blog
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "build-blog";
-          src = ./.;
-          buildInputs = dependencies;
-          buildPhase = ''
-            quarto render
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r _site/* $out/
-          '';
-        };
+        # packages.default = pkgs.stdenv.mkDerivation {
+        #   name = "build-blog";
+        #   src = ./.;
+        #   buildInputs = dependencies;
+        #   QUARTO_PYTHON = "./.venv/bin/python";
+        #   buildPhase = ''
+        #     quarto render
+        #   '';
+        #   installPhase = ''
+        #     mkdir -p $out
+        #     cp -r _site/* $out/
+        #   '';
+        # };
 
-      });
+      }
+    );
 }
