fc88dadf6b80a14bd2d2504521bfca8e02685f85
Luca Leon Happel <lucahappel99@gmx.de>
2026-06-21 22:05:13 +0200

    add: haskell kernel

--- flake.nix
+++ flake.nix
@@ -58,6 +58,15 @@
           # lean4
           # (python3.withPackages pythonPackages)
           uv
+          (haskellPackages.ghcWithPackages (
+            p: with p; [
+              ihaskell
+              ihaskell-magic
+              ihaskell-basic
+              ihaskell-hatex
+              # no external packages so far...
+            ]
+          ))
         ];
       in
       {
@@ -76,27 +85,29 @@
               else
                 return
               fi
-              # if "lean4" is missing from the output of `jupyter kernelspec
-              # list`, add it
-              if [! jupyter kernelspec list | grep -q "lean4"]; then
-                echo "Adding lean4 kernel to Jupyter"
-                uv run python -m lean4_jupyter.install
-              fi
-              # check if `/.lean4_jupyter/repl` exists
-              if [ ! -f "$HOME/.lean4_jupyter/repl" ]; then
-                echo "Installing lean4 repl"
-                mkdir -p .lean4_jupyter
-                git clone https://github.com/leanprover-community/repl .lean4_jupyter/repl
-                cd .lean4_jupyter/repl
-                git checkout adbbfcb9d4e61c12db96c45d227de92f21cc17dd
-                lake build
-                cd ../..
-              fi
-              # add .lean4_jupyter/repl/.lake/build/bin/repl  to PATH (from cwdA)
-              export PATH="$PATH:$(pwd)/.lean4_jupyter/repl/.lake/build/bin"
               ${pkgs.onefetch}/bin/onefetch
               uv sync
               source .venv/bin/activate
+              # if "lean4" is missing from the output of `jupyter kernelspec
+              # list`, add it
+              jupyter kernelspec list | grep -q "lean4" \
+                || {
+                  echo "installing Lean4 kernel" ;
+                  uv run python -m lean4_jupyter.install --sys-prefix
+                }
+              jupyter kernelspec list | grep -q "ihaskell" \
+                || { 
+                  echo "installing Haskell kernel" ;
+                  ihaskell install --ghclib="$(ghc --print-libdir)" --prefix=".venv/";
+                }
+              [ -d ".lean4_jupyter/repl" ] || {
+                echo "Installing lean4 repl";
+                mkdir -p .lean4_jupyter;
+                git clone https://github.com/leanprover-community/repl .lean4_jupyter/repl;
+                (cd .lean4_jupyter/repl && git checkout adbbfcb9d4e61c12db96c45d227de92f21cc17dd && lake build);
+              }
+              # add .lean4_jupyter/repl/.lake/build/bin/repl  to PATH (from cwdA)
+              export PATH="$PATH:$(pwd)/.lean4_jupyter/repl/.lake/build/bin"
               trap deactivate EXIT
             '';
           };
